78ad948b8b42ab6d5599e261b1fc2162aaa1e3a7
aristore018-cpu <aristore018@gmail.com>
2026-07-04 02:42:30 +0800

    Change server port to 3000
    Set the server port to a fixed value of 3000.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000; // <- PAKE PORT YANG SAMA DENGAN DI RAILWAY
 
 app.use(cors());
 app.use(express.json());
